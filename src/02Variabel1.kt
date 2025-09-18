b7351b960ffbddf6e5033bc15c1a291a59dcb41a
agungsetiawan-afk <agungbka12@gmail.com>
2025-09-18 08:21:17 +0700

    Variabel1

--- src/02Variabel1.kt
+++ src/02Variabel1.kt
@@ -1,7 +1,4 @@
 fun main(args: Array<String>) {
     var dynamicVar = 2
     println("Contoh dynamic variabel = " + dynamicVar)
-
-    var staticVar: Char = 'A'
-    println("Contoh static variabel = " + staticVar)
 }
